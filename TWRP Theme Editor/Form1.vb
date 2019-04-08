aa24059ebb72d9f4dd4d34073b13fc18b8a60974
Devil7DK <dineshthangavel47@gmail.com>
2019-04-08 08:40:05 +0530

    Add project files.

---
+++ TWRP Theme Editor/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+
+
+Partial Public Class Form1
+    Shared Sub New()
+        DevExpress.UserSkins.BonusSkins.Register()
+    End Sub
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+End Class
